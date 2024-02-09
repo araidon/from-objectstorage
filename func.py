03c36b4afb8472694fc648a6291fe905b5099e28
araidon <arai715work@gmail.com>
2024-02-09 10:12:43 +0900

    toriaezu

--- func.py
+++ func.py
@@ -13,26 +13,39 @@ def handler(ctx, data: io.BytesIO = None):
     except (Exception, ValueError) as ex:
         logging.getLogger().info('error parsing json payload: ' + str(ex))
     
+    # ファイル名、バケット名、名前空間を取得
     FILENAME = json.dumps(body.get("data").get("resourceName"))
     BUCKET_NAME = json.dumps(body.get("data").get("additionalDetails").get("bucketName"))
     NAMESPACE = json.dumps(body.get("data").get("additionalDetails").get("namespace"))
 
-    # logging.getLogger().info(f"FILENAME={FILENAME}, BUCKET_NAME={BUCKET_NAME}, NAMESPACE={NAMESPACE}")
-    print(f"FILENAME={FILENAME}, BUCKET_NAME={BUCKET_NAME}, NAMESPACE={NAMESPACE}")
+    logging.getLogger().info(f"FILENAME={FILENAME}, BUCKET_NAME={BUCKET_NAME}, NAMESPACE={NAMESPACE}")
+    # print(f"FILENAME={FILENAME}, BUCKET_NAME={BUCKET_NAME}, NAMESPACE={NAMESPACE}")
 
+    # リソースプリンシパルの署名者を取得
     signer = oci.auth.signers.get_resource_principals_signer()
     object_storage = oci.object_storage.ObjectStorageClient(config={}, signer=signer)
     #object_storage = oci.object_storage.ObjectStorageClient(config)
 
+    # オブジェクトの取得
     #object = object_storage.get_object(NAMESPACE,BUCKET_NAME,FILENAME)
-    object = object_storage.list_objects(NAMESPACE,BUCKET_NAME)
+    #object = object_storage.list_objects(NAMESPACE,BUCKET_NAME)
 
-    objects = [b.name for b in object.data.objects]
+    #objects = [b.name for b in object.data.objects]
 
-    logging.getLogger().info(objects)
+    #logging.getLogger().info(object)
 
     return response.Response(
         ctx, response_data=json.dumps(
             {"message": "Hello {0}".format(name)}),
         headers={"Content-Type": "application/json"}
     )
+
+#Local Debugging
+if __name__ == "__main__":
+    import io
+    import json
+    from fdk import response
+
+    with open("sample.json", "rb") as f:
+        resp = handler(None, io.BytesIO(f.read()))
+        print(resp)
